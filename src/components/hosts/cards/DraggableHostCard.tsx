4dbba3f4d0fa2a33d05c416649d498d54d328582
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2026-08-14 15:36:41 +0600

    style: increase host card logo opacity

--- src/components/hosts/cards/DraggableHostCard.tsx
+++ src/components/hosts/cards/DraggableHostCard.tsx
@@ -124,7 +124,7 @@ export function DraggableHostCard({
         </div>
       )}
 
-      <div className="absolute -bottom-3 -right-3 pointer-events-none opacity-20 group-hover:opacity-30 transition-opacity">
+      <div className="absolute -bottom-3 -right-3 pointer-events-none opacity-30 group-hover:opacity-40 transition-opacity">
         <OsIcon os={host.os} className="w-20 h-20 text-dark-400" />
       </div>
 
